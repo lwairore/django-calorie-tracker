d472424c790dd05be2fa58de7f80c93f8140d4ad
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 15:52:09 +0300

    - Configure the name of the app.

--- calorie_tracker_app/urls.py
+++ calorie_tracker_app/urls.py
@@ -1,5 +1,8 @@
+from calorie_tracker_app.apps import CalorieTrackerAppConfig
 from django.urls import path
 from .views import index
+
+app_name = CalorieTrackerAppConfig.name
 
 urlpatterns = [
     path('', index, name="index"),
