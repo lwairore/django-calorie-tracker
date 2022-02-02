eaea50c9d760726e703a61ad48e16b0fb2081013
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 16:23:59 +0300

    - Map 'URLConfs' for view 'delete_consume'.

--- calorie_tracker_app/urls.py
+++ calorie_tracker_app/urls.py
@@ -1,9 +1,10 @@
 from calorie_tracker_app.apps import CalorieTrackerAppConfig
 from django.urls import path
-from .views import index
+from .views import index,delete_consume
 
 app_name = CalorieTrackerAppConfig.name
 
 urlpatterns = [
     path('', index, name="index"),
+    path('', delete_consume, name="delete_consume"),
 ]
