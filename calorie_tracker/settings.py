e4084bdcd82d1b3ca5de2173c9b472e683223024
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 15:15:14 +0300

    - Register app 'calorie_tracker_app'.

--- calorie_tracker/settings.py
+++ calorie_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calorie_tracker_app.apps.CalorieTrackerAppConfig',
 ]
 
 MIDDLEWARE = [
