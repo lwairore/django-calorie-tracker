08e544af2fed6171273dcfd38ae2667bb209fa64
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 16:06:59 +0300

    - Register model 'ConsumeModel' on the admin site.

--- calorie_tracker_app/admin.py
+++ calorie_tracker_app/admin.py
@@ -1,5 +1,6 @@
-from calorie_tracker_app.models import FoodModel
+from calorie_tracker_app.models import ConsumeModel, FoodModel
 from django.contrib.admin import register
 
 # Register your models here.
 register(FoodModel)
+register(ConsumeModel)
