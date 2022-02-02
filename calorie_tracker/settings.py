5905a07fcdd7dc4fd10f4a5bf8e973c888d5e751
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 15:20:14 +0300

    - Configure the project to use the database configuration stored as environments.

--- calorie_tracker/settings.py
+++ calorie_tracker/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'calorie_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD'),
+        'HOST': os.environ.get('SQL_HOST'),
+        'PORT': os.environ.get('SQL_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- calorie_tracker_app/models.py
+++ calorie_tracker_app/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.db.models import Model
+from django.db.models.fields import (CharField, FloatField, IntegerField)
 
 # Create your models here.
+class FoodModel(Model):
+    
+    name = CharField(max_length=100)
+    carbs = FloatField()
+    protein = FloatField()
+    fats = FloatField()
+    calories = IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Food'
+        verbose_name_plural = 'Foods'
+        
