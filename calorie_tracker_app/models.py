afbd5448251f5d5a1f03ff4e22e19da66b8329e0
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 15:37:10 +0300

    - Create model Consume

--- calorie_tracker_app/models.py
+++ calorie_tracker_app/models.py
@@ -1,5 +1,6 @@
-from django.db.models import Model
+from django.db.models import Model, ForeignKey, CASCADE
 from django.db.models.fields import (CharField, FloatField, IntegerField)
+from django.contrib.auth.models import User
 
 # Create your models here.
 class FoodModel(Model):
@@ -17,3 +18,13 @@ class FoodModel(Model):
         verbose_name = 'Food'
         verbose_name_plural = 'Foods'
         
+class Consume(Model):
+    user = ForeignKey(User, on_delete=CASCADE)
+    food_consumed = ForeignKey(FoodModel, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Consume'
+        verbose_name_plural = 'Consumes'
